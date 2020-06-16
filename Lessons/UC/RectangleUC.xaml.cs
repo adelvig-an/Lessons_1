e5d7fd5180137794a30f52db5a6cf3065378392b
adelvig-an <63202963+adelvig-an@users.noreply.github.com>
2020-06-16 22:46:52 +0700

    lesson1

---
+++ Lessons/UC/RectangleUC.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lessons.UC
+{
+    /// <summary>
+    /// Логика взаимодействия для RectangleUC.xaml
+    /// </summary>
+    public partial class RectangleUC : UserControl
+    {
+        public RectangleUC()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Lessons/UC/SquareUC.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lessons.UC
+{
+    /// <summary>
+    /// Логика взаимодействия для SquareUC.xaml
+    /// </summary>
+    public partial class SquareUC : UserControl
+    {
+        public SquareUC()
+        {
+            InitializeComponent();
+        }
+    }
+}
