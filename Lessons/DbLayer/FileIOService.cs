f8c6faf5df69b8f8cbabce753a31c881a43611e0
adelvig-an <63202963+adelvig-an@users.noreply.github.com>
2020-06-18 17:23:45 +0700

    lesson1

---
+++ Lessons/DbLayer/FileIOService.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using Lessons.VM;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lessons.DbLayer
+{
+    class FileIOService
+    {
+        private readonly string PATH;
+
+        public FileIOService(string path)
+        {
+            PATH = path;
+        }
+
+        public PageVM LoadData()
+        {
+            var fileExists = File.Exists(PATH);
+            if (!fileExists)
+            {
+                File.CreateText(PATH).Dispose();
+                return new PageVM();
+            }
+            using (var reader = File.OpenText(PATH))
+            {
+                var fileText = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<PageVM>(fileText);
+            }
+        }
+
+        public void SaveData(PageVM pageVM)
+        {
+            using (StreamWriter write = File.CreateText(PATH))
+            {
+                string output = JsonConvert.SerializeObject(pageVM);
+                write.Write(output);
+            }
+        }
+    }
+}
